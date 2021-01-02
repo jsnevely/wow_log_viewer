f7571d536f76facbda71ed36ce2c679d316a080d
Joe Snevely <jsnevely@gmail.com>
2021-01-01 19:27:24 -0500

    fun with lines

--- LogEntry.js
+++ LogEntry.js
@@ -1,13 +1,8 @@
 class LogEntry{
     constructor(_str) {
-        _str = _str.split('\r\n').filter(a => a.length > 0);
-        if(_str.length) {
-            _str = _str[0];
-            const DATE_SPLIT = _str.indexOf('  ');
-            this.date = new Date(_str.substring(0, DATE_SPLIT));
-            let _data = _str.substring(DATE_SPLIT + 2).replace(/"/g, '').split(',');
-            this.data = _data
-        }
+        const DATE_SPLIT = _str.indexOf('  ');
+        let _data = _str.substring(DATE_SPLIT + 2).replace(/"/g, '').split(',');
+        this.data = _data;
     }
 };
 
--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const path = require('path');
 const LOG_PATH = path.join(process.env[`ProgramFiles(x86)`], `World of Warcraft`, `_retail_`, `Logs`, `WoWCombatLog.txt`);
 const TailingReadableStream = require('tailing-stream');
 const LogEntry = require('./LogEntry');
+const LineByLine = require('n-readlines');
+
 let entries = [];
 
 async function main() {
     try {
         let len = await checkFile();
+        initializeFile();
         await readFile(len);
     } catch (ex) {
         throw ex;
@@ -18,7 +21,7 @@ function checkFile() {
     return new Promise((resolve, reject) => {
         fs.stat(LOG_PATH, (err, stats) => {
             if(err) return reject(err);
-            getFileLength().then(resolve).catch(reject);
+getFileLength().then(resolve).catch(reject);
         });
     });
 }
@@ -27,30 +30,41 @@ function getFileLength() {
     return new Promise((resolve, reject) => {
         fs.readFile(LOG_PATH, 'utf-8', (err, data) => {
             if(err) return reject(err);
-            return resolve(data.length);
-            // return resolve(JSON.stringify(data.toString()).length - 1);
+            return resolve(Buffer.byteLength(data, 'utf-8'));
         });
     });
 }
 
+function initializeFile() {
+        let file_length = 0;
+        const liner = new LineByLine(LOG_PATH);
+
+        let line;
+        let line_number = 0;
+        
+        while(line = liner.next()) {
+            let data = line.toString('ascii');
+            file_length += data.length;
+            line_number++;
+            entries.push(new LogEntry(data));
+        }
+
+        console.log(`file_length: ${file_length}, lines: ${line_number}`);
+        return file_length;
+}
+
 function readFile(len) {
     return new Promise((resolve, reject) => {
-        let initial_read = true;
         console.log('reading file');
-        const stream = TailingReadableStream.createReadStream(LOG_PATH, {timeout: 0, start: 0});
+        const stream = TailingReadableStream.createReadStream(LOG_PATH, {timeout: 0, start: len});
 
         stream.on('data', buffer => {
-            let data = buffer.toString()
-            if(initial_read) {
-                data = data.split('\r\n');
-                entries = data.map(e => new LogEntry(e));
-                initial_read = false;
-            } else {
-                let entry = new LogEntry(data);
-                if(entry) {
-                    console.log(entry);
-                    entries.push(entry);
-                }
+            let data = buffer.toString();
+            data = data.split('\r\n').filter(a => a.length > 0);
+            for(let d of data) {
+                let e = new LogEntry(d);
+                console.log(e);
+                entries.push(e)
             }
         });
 
@@ -64,6 +78,6 @@ function readFile(len) {
 
 main().then(() => {
     process.exit(0);
-}).catch(() => {
-    process.exit(1);
+}).catch((ex) => {
+    throw ex;
 })
